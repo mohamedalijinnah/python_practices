dc82566dc3f2eba0b38e32131c44ac4df7ac8030
mohamedalijinnah <43814278+mohamedalijinnah@users.noreply.github.com>
2025-01-24 23:23:22 +0000

    drawShapes

---
+++ drawShapes.py
@@ -0,0 +1,32 @@
+# Python code​​​​​​‌‌​​‌​​​​​‌​​​‌‌​‌‌‌‌‌‌​‌ below
+class Shape:
+	width = 5
+	height = 5
+	printChar = '#'
+
+	def printRow(self, i):
+		raise NotImplementedError("Will be implemented by children extending this class")
+
+	def print(self):
+		for i in range(self.height):
+			self.printRow(i)
+
+
+class Square(Shape):
+	def printRow(self, i):
+		print(self.printChar * self.width)
+
+class Triangle(Shape):
+	height = 5
+	width = 2 * height
+	def printRow(self, i):
+		triangle = i * 2 + 1
+		padding = int((self.width - triangle)/2)
+		print(' '*padding + self.printChar * triangle)
+	pass
+
+result = Square()
+result.print()
+
+result = Triangle()
+result.print()
